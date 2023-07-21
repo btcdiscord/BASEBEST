8de12f7064a05a85befa56faf2e79fccca4c521a
btcdiscord <140143323+btcdiscord@users.noreply.github.com>
2023-07-21 09:16:22 +0200

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x3cdb5d33b2391f9b93d05517d2bf7b7404c00ad2";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
